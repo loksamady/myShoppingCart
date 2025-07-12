71fd162ee1b632196c73e35d041295260779f1aa
Lok Samady <samady.lok@csx.com.kh>
2025-07-12 21:25:45 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   const { allItems, setItems } = UseCartContext();
   useEffect(() => {
     setItems();
-  });
+  }, []);
   // useEffect(() => {
   //   console.log(allItems);
   // });
